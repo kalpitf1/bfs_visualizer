5e049daf8bc6381770acd8c834788bd6ee332ef6
kalpitfulwariya <kalpit.fulwariya@kaleidoscope-blockchain.com>
2024-10-15 21:56:27 +0530

    fix: remove wildcard, only allow request from react app

--- backend/main.go
+++ backend/main.go
@@ -33,7 +33,7 @@ func main() {
 
 	// Apply CORS headers to the router
 	cors := handlers.CORS(
-		handlers.AllowedOrigins([]string{"*"}), // Allow requests from your React app
+		handlers.AllowedOrigins([]string{"https://bfsvis.netlify.app/"}), // Allow requests from your React app
 		handlers.AllowedMethods([]string{"POST"}),
 		handlers.AllowedHeaders([]string{"Content-Type"}),
 	)
